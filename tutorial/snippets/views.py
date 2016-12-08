58896abd60ca43efc090c4e01ac520862b2e2af3
Diego16 <diegogb0113@gmail.com>
2016-12-08 17:24:07 -0500

    Addition of User model views serializer and permissions, Added required permissions to views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,12 +1,24 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from snippets.serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, permissions
+from snippets.permissions import IsOwnerOrReadOnly
+class UserList(generics.ListAPIView):
+	queryset= User.objects.all()
+	serializers_class= UserSerializer
+class UserDetail(generics.RetrieveAPIView):
+	queryset= User.objects.all()
+	serializer_class=UserSerializer
 class SnippetList(generics.ListCreateAPIView):
+	permission_classes=(permissions.IsAuthenticatedOrReadOnly,)
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+	permission_classes=(permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
