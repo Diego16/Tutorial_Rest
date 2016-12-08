43440ddd3d260823ac9d45992b860bdcfe2ab1a8
Diego16 <diegogb0113@gmail.com>
2016-12-08 16:23:53 -0500

    Modifications in models.py, old migrations and DB deleted, new migration with 2 new attributes in Snippet, minor corrections

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
-class SnippetList(generics.ListCreateModelAPIView):
+class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
