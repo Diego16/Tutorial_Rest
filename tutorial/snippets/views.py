422935c8f7705a4ded769f698fac22faa3470baf
Diego16 <diegogb0113@gmail.com>
2016-12-09 12:41:32 -0500

    Creation of API's root in views.py, added urlpatterns for root in snippets/urls.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,10 +2,18 @@ from snippets.models import Snippet
 from django.contrib.auth.models import User
 from snippets.serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics, permissions
+from rest_framework.reverse import reverse
+from rest_framework import generics, permissions, renderers
 from snippets.permissions import IsOwnerOrReadOnly
+@api_view(['GET'])
+def api_root(request, format=None):
+	return Response({
+		'users': reverse('user-list', request=request, format=format),
+		'snippets': reverse('snippet-list', request=request, format=format)
+	})
 class UserList(generics.ListAPIView):
 	queryset= User.objects.all()
 	serializers_class= UserSerializer
@@ -22,3 +30,9 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	permission_classes=(permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	renderer_classes= (renderers.StaticHTMLRenderer,)
+	def get(self, request, *args, **kwargs):
+		snippet= self.get_object()
+		return Response(snippet.highlighted)
